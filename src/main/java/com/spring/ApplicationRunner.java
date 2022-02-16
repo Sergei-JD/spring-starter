7d048dfb7ba74fd83915aaab53bd1c56129d3deb
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-16 10:40:30 +0300

    added Factory Method Injection

--- src/main/java/com/spring/ApplicationRunner.java
+++ src/main/java/com/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.spring;
 
 import com.spring.database.database.pool.ConnectionPool;
+import com.spring.database.database.repisitory.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -10,5 +11,8 @@ public class ApplicationRunner {
         // clazz -> String -> Map<String, Object>
         var connectionPool = context.getBean("p1", ConnectionPool.class);
         System.out.println(connectionPool);
+
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
