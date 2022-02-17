03ea4adf743ae0e5b9a7769af192c52a40620f1d
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-17 12:13:17 +0300

    implemented @Autowired & @Value

--- src/main/java/com/spring/database/repisitory/CompanyRepository.java
+++ src/main/java/com/spring/database/repisitory/CompanyRepository.java
@@ -1,20 +1,44 @@
 package com.spring.database.repisitory;
 
 import com.spring.bpp.Auditing;
-import com.spring.bpp.InjectBean;
 import com.spring.bpp.Transaction;
 import com.spring.database.entity.Company;
 import com.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+//    // var1
+//    @Resource(name = "pool1")
+//    private ConnectionPool connectionPool;
+
+//    // var2
+//    @Autowired
+//    @Qualifier("pool1")
+//    private ConnectionPool connectionPool;
+
+//    // var3
+//    @Autowired
+//    private ConnectionPool pool1;
+
+    // var4
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
+
+    @Autowired
+    public void setPool1(ConnectionPool pool1) {
+        this.pool1 = pool1;
+    }
 
     @PostConstruct
     private void init() {
