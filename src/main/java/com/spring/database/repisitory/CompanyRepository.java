8954c0f45879c3bfe05e99ca017844b5ad20744b
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-17 09:42:54 +0300

    added Annotation-based Configuration;  Custom BeanPostProcessor(postProcessBeforeInitialization method)

--- src/main/java/com/spring/database/repisitory/CompanyRepository.java
+++ src/main/java/com/spring/database/repisitory/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.spring.database.repisitory;
 
+import com.spring.bpp.InjectBean;
 import com.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
