962edf21a397dc0d41f23cb70f5eabd9123a603b
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-18 11:07:36 +0300

    added Event Listeners

--- src/main/java/com/spring/ApplicationRunner.java
+++ src/main/java/com/spring/ApplicationRunner.java
@@ -3,6 +3,7 @@ package com.spring;
 import com.spring.config.ApplicationConfiguration;
 import com.spring.database.pool.ConnectionPool;
 import com.spring.database.repository.CrudRepository;
+import com.spring.service.CompanyService;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -24,8 +25,8 @@ public class ApplicationRunner {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
 
-            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
-            System.out.println(companyRepository.findById(1));
+            var companyService = context.getBean(CompanyService.class);
+            System.out.println(companyService.findById(1));
         }
     }
 }
