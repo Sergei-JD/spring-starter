043de6cce64fcab108fe3dc99779c5fb9f831582
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-18 09:23:58 +0300

    added @Bean

--- src/main/java/com/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/spring/config/ApplicationConfiguration.java
@@ -1,20 +1,26 @@
 package com.spring.config;
 
+import com.spring.database.pool.ConnectionPool;
 import com.spring.database.repository.CrudRepository;
+import com.spring.database.repository.UserRepository;
 import com.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
-@Configuration
+@Configuration(proxyBeanMethods = true)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.spring",
 useDefaultFilters = false,
@@ -24,4 +30,29 @@ includeFilters = {
         @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
 })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        var connectionPool1 = pool3();
+        var connectionPool2 = pool3();
+        var connectionPool3 = pool3();
+        return new UserRepository(pool3());
+    }
 }
