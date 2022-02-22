b097ceb40264fa2a3c17b8787046af9aeb3992ea
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-22 10:47:08 +0300

    implemented Data JPA Starter: Hibernate Entities

--- src/main/java/com/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/spring/database/repository/CompanyRepository.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,9 +25,7 @@ import java.util.Optional;
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
     private final ConnectionPool pool1;
-
     private final List<ConnectionPool> pools;
-
     @Value("${db.pool.size}")
     private final Integer poolSize;
 
@@ -38,7 +37,7 @@ public class CompanyRepository implements CrudRepository<Integer, Company> {
     @Override
     public Optional<Company> findById(Integer id) {
         System.out.println("findById method...");
-        return Optional.of(new Company(id));
+        return Optional.of(new Company(id, null, Collections.emptyMap()));
     }
 
     @Override
